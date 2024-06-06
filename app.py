20654f142db50b6b3f99833227978cb90bf6d3d9
CARLOSMEYER <20210696@uthh.edu.mx>
2024-06-06 15:07:21 -0600

    Mensaje de commit aquí

--- app.py
+++ app.py
@@ -1,27 +1,43 @@
-from flask import Flask, render_template, request
+from flask import Flask, request, render_template, jsonify
+import joblib
+import pandas as pd
+import logging
+import sklearn
 
 app = Flask(__name__)
 
+# Configurar el registro
+logging.basicConfig(level=logging.DEBUG)
+
+# Cargar el modelo entrenado
+model = joblib.load('model.pkl')
+app.logger.debug('Modelo cargado correctamente.')
+
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('formulario.html')
 
-@app.route('/calculate', methods=['POST'])
-def calculate():
-    num1 = float(request.form['num1'])
-    num2 = float(request.form['num2'])
-    operation = request.form['operation']
-    
-    if operation == 'add':
-        result = num1 + num2
-    elif operation == 'subtract':
-        result = num1 - num2
-    elif operation == 'multiply':
-        result = num1 * num2
-    elif operation == 'divide':
-        result = num1 / num2
-    
-    return render_template('index.html', result=result)
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        # Obtener los datos enviados en el request
+        abdomen = float(request.form['abdomen'])
+        antena = float(request.form['antena'])
+        
+        # Crear un DataFrame con los datos
+        data_df = pd.DataFrame([[abdomen, antena]], columns=['abdomen', 'antena'])
+        app.logger.debug(f'DataFrame creado: {data_df}')
+        
+        # Realizar predicciones
+        prediction = model.predict(data_df)
+        app.logger.debug(f'Predicción: {prediction[0]}')
+        
+        # Devolver las predicciones como respuesta JSON
+        return jsonify({'categoria': prediction[0]})
+    except Exception as e:
+        app.logger.error(f'Error en la predicción: {str(e)}')
+        return jsonify({'error': str(e)}), 400
 
 if __name__ == '__main__':
     app.run(debug=True)
+
